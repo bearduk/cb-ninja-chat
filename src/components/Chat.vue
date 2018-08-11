3b22dd60565eb85f64d7a77aac697c080c752570
Chris Beard <coebe500@aol.com>
2018-08-11 11:02:56 +0100

    Vue-chat-scroll added. Also getting instant snapshot changes from docChanges() method. Outputting dates using moment-js.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,11 +4,11 @@
         <!-- <button @click.prevent="test">test</button> -->
         <div class="card">
             <div class="card-content">
-                <ul class="messages">
-                    <li>
-                        <span class="teal-text">Name</span>
-                        <span class="grey-text text-darken-3">message</span>
-                        <span class="grey-text time">time</span>
+                <ul class="messages" v-chat-scroll>
+                    <li v-for="message in messages" :key="message.id">
+                        <span class="teal-text">{{message.name}}</span>
+                        <span class="grey-text text-darken-3">{{message.content}}</span>
+                        <span class="grey-text time">{{message.timestamp}}</span>
                     </li>
                 </ul>
             </div>
@@ -19,7 +19,9 @@
 </template>
 
 <script>
-    import NewMessage from '@/components/NewMessage';
+    import NewMessage from '@/components/NewMessage'
+    import db from '@/firebase/init'
+    import moment from "moment"
 
     export default {
         name: 'Chat',
@@ -32,7 +34,26 @@
         data(){
             return {
                 // uname: this.$route.params.uname
+                messages: []
             }
+        },
+        created() {
+            let ref = db.collection('chat-messages').orderBy('timestamp');
+
+            ref.onSnapshot(snapshot => {
+                // console.log(snapshot.docChanges());
+                snapshot.docChanges().forEach(change => {
+                    if (change.type == 'added'){
+                        let doc = change.doc
+                        this.messages.push({
+                            id: doc.id,
+                            name: doc.data().name,
+                            content: doc.data().content,
+                            timestamp: moment(doc.data().timestamp).format('llll')
+                        })
+                    }
+                })
+            })
         },
         methods: {
             // test() {
@@ -52,6 +73,13 @@
 }
 .chat .time {
     display: block;
-    font-size: 1.2rem;
+    font-size: .8rem;
+}
+.messages {
+    max-height: 300px;
+    overflow: auto;
+}
+.message::-webkit-scrollbar {
+
 }
 </style>
